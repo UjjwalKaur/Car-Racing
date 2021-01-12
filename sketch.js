33dc6018424157f59af8f263b0d0befab3d7dfaa
UjjwalKaur <75835619+UjjwalKaur@users.noreply.github.com>
2021-01-12 14:45:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,13 +12,13 @@ var cars, car1, car2, car3, car4;
 var car1Image, car2Image, car3Image, car4Image, groundImage, trackImage;
 
 function preload(){
-  car1Image = loadImage("images/car1.png");
-  car2Image = loadImage("images/car2.png");
-  car3Image = loadImage("images/car3.png");
-  car4Image = loadImage("images/car4.png");
+  car1Image = loadImage("car1.png");
+  car2Image = loadImage("car2.png");
+  car3Image = loadImage("car3.png");
+  car4Image = loadImage("car4.png");
   
-  groundImage = loadImage("images/ground.png");
-  trackImage = loadImage("images/track.png");
+  groundImage = loadImage("ground.png");
+  trackImage = loadImage("track.jpg");
 }
 
 
